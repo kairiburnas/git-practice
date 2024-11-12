bacafa137fea5506bcd155ec04f5e4bbecdefd56
kairiburnas <kairi.burnasheva@gmail.com>
2024-11-12 10:49:15 +0200

    New code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Hello universe!");
+        String message = "Welcome to the universe";
+        System.out.println(message);
+
+        int number = 10;
+        System.out.println(number);
     }
 }
